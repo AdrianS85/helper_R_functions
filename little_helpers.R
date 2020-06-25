8c44c1604756347c64dce584f29d71e3c14d3a68
AdrianS85 <adrianstankiewicz85@gmail.com>
2020-06-25 16:50:16 +0200

    Update little_helpers.R

--- little_helpers.R
+++ little_helpers.R
@@ -203,16 +203,16 @@ remove_corrupting_symbols_from_chrvec <- function(chr_vec, repeated_spaces, trai
   if(class(chr_vec) == 'character'){
     if (repeated_spaces == T) {
       chr_vec <- stringr::str_replace_all(string = chr_vec, pattern = ' {2,}', replacement = ' ')}
-
+    
     if (trailing_spaces == T) {
       chr_vec <- stringr::str_trim(string = chr_vec, side = 'both')}
-
+    
     if (character_NAs == T) {
       chr_vec[chr_vec == 'NA'] <- NA}
-
-     if (change_to_lower == T) {
-       chr_vec <- tolower(chr_vec)}
-
+    
+    if (change_to_lower == T) {
+      chr_vec <- tolower(chr_vec)}
+    
     return(chr_vec)
   } else { 
     warning('Supplied vector is not of class character!')
